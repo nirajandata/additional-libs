cd035eb4a084ad1eab5af98a480d06a6debfc1f4
Nirajan Dhakal <nirajan.data@gmail.com>
2023-06-25 05:09:42 -0400

    baniya:1.0

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cpp-env-var
 ### A small C++ library for handling environmenal variable 
+It will have two features:
+1. environmental variable from file (by default, it will be .env)
+2. It will fetch environmental variable from the shell
--- getenv.h
+++ getenv.h
@@ -5,4 +5,46 @@
 #ifndef CPP_ENV_VAR_GETENV_H
 #define CPP_ENV_VAR_GETENV_H
 
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <algorithms>
+#include <array>
+#include <tuple>
+
+std::tuple<string,string> trim(std::string &line){
+    std::erase(std::remove(line.begin(),line.end(),' '),line.end());
+    array<int,4> locate;
+    int count=0;
+    for(size_t i=0;i<line.size();i++){
+        if(line[i]=="'" or line[i]=='"'){
+            locate[count]=i;
+            count++;
+        }
+        assert(("Please! Only put quotes in the proper place, not inside key or value",count<4));
+    }
+    std::string key, value;
+    key=line.substr(locate[0]+1,locate[1]-locate[0]);
+    value=line.substr(locate[2]+1, locate[3]-locate[2]);
+    return make_pair(key,value)
+}
+std::map<string,string> get_env_fromFile(std::string path=".env"){
+    std::ifstream file(path);
+    //we store the key and value in dictionary called baniya
+    std::map<string,string> baniya;
+    std::string line;
+
+    while(getline(file,line)){
+        std::string key, value;
+       std::tie(key,value) =trim(line);
+       baniya[key]=value;
+    }
+
+    file.close();
+    return baniya;
+}
+
+std::string get_env_fromOS(std::string token_name){
+
+}
 #endif //CPP_ENV_VAR_GETENV_H
