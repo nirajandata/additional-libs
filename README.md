9fbbabf50b5a8c09700b318f1287c7c7e5d157e1
Nirajan Dhakal <nirajan.data@gmail.com>
2023-06-25 06:32:23 -0400

    final version

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # cpp-env-var
 ### A small C++ library for handling environmenal variable 
+
+```I know that it's very easy and simple but I will it it for personal use as it's annoying for me to code to read .env everytime```
+
 It will have two features:
 1. environmental variable from file (by default, it will be .env)
-2. It will fetch environmental variable from the shell
+2. ~~It will fetch environmental variable from the OS~~ (No no, I won't be writing for that, just use std::getenv())
+   (But if someone want to contribute the second part,I'm down. You can send pull requests for that ^-^ )
+
+Sample code :
+
+```cpp
+#include "getenv.h"
+
+int main(){
+   std::map<std::string,std::string> h= get_env_fromFile();
+   std:: cout << h["hello"];
+}
+
+```
+
+Sample env file :
+Remember to use quotes(single/double) for both keys and values
+```yaml
+"test"="nirajan"
+" hello "  = "world"
+```
--- getenv.h
+++ getenv.h
@@ -8,44 +8,47 @@
 #include <iostream>
 #include <fstream>
 #include <map>
-#include <algorithms>
+#include <algorithm>
 #include <array>
 #include <tuple>
+#include <cassert>
 
-std::tuple<string,string> trim(std::string &line){
-    std::erase(std::remove(line.begin(),line.end(),' '),line.end());
-    array<int,4> locate;
+std::tuple<std::string,std::string> trim(std::string &line){
+    line.erase(std::remove(line.begin(),line.end(),' '),line.end());
+    std::array<int,4> locate;
     int count=0;
     for(size_t i=0;i<line.size();i++){
-        if(line[i]=="'" or line[i]=='"'){
-            locate[count]=i;
+        if(line[i]=='\'' or line[i]=='"'){
+            locate[count]=i+1;
+            if(count&1)  locate[count]--;
+
             count++;
         }
-        assert(("Please! Only put quotes in the proper place, not inside key or value",count<4));
     }
+    assert(("Please! Only put quotes in the proper place, not inside key or value" && count==4));
     std::string key, value;
-    key=line.substr(locate[0]+1,locate[1]-locate[0]);
-    value=line.substr(locate[2]+1, locate[3]-locate[2]);
-    return make_pair(key,value)
+    key=line.substr(locate[0],locate[1]-locate[0]);
+    value=line.substr(locate[2], locate[3]-locate[2]);
+    return make_pair(key,value);
 }
-std::map<string,string> get_env_fromFile(std::string path=".env"){
+std::map<std::string,std::string> get_env_fromFile(std::string path=".env"){
     std::ifstream file(path);
     //we store the key and value in dictionary called baniya
-    std::map<string,string> baniya;
+    std::map<std::string,std::string> baniya;
     std::string line;
 
     while(getline(file,line)){
         std::string key, value;
        std::tie(key,value) =trim(line);
        baniya[key]=value;
+       //std::cout << key << "|"<< value << std::endl;
     }
 
     file.close();
     return baniya;
 }
 
-//Kept as TODO for somedays as I have some homeworks to do for college
-std::string get_env_fromOS(std::string token_name){
+//implementing for os version is very messy, so for this, just use std::getenv()
 
-}
+
 #endif //CPP_ENV_VAR_GETENV_H
