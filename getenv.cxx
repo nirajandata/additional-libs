4783335a06603998c3fd6497804a4b637ed6aff0
nirajandata <nirajan.data@gmail.com>
2023-11-17 03:13:13 -0500

    c++ 20 module support

--- getenv.cxx
+++ getenv.cxx
@@ -1,11 +1,19 @@
-#include "getenv.h"
+module;
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <map>
 
-std::tuple<std::string, std::string> trim(std::string &line) {
+export module modenv;
+export namespace env {
+
+[[nodiscard]] auto trim(std::string &line) noexcept {
   line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
   std::array<int, 4> locate;
-  int count = 0;
+  size_t count = 0;
   for (size_t i = 0; i < line.size(); i++) {
-    if (line[i] == '\'' or line[i] == '"') {
+    if (line[i] == '\'' || line[i] == '"') {
       locate[count] = i + 1;
       if (count & 1)
         locate[count]--;
@@ -13,27 +21,22 @@ std::tuple<std::string, std::string> trim(std::string &line) {
       count++;
     }
   }
-  assert(
-      ("Please! Only put quotes in the proper place, not inside key or value" &&
-       count == 4));
   std::string key, value;
   key = line.substr(locate[0], locate[1] - locate[0]);
   value = line.substr(locate[2], locate[3] - locate[2]);
-  return make_pair(key, value);
+  return std::array<std::string, 2>{key, value};
 }
-std::map<std::string, std::string> get_env_fromFile(std::string path) {
+
+[[nodiscard]] auto getenv(std::string path = ".env") noexcept {
   std::ifstream file(path);
   // we store the key and value in dictionary called baniya
   std::map<std::string, std::string> baniya;
   std::string line;
 
   while (getline(file, line)) {
-    std::string key, value;
-    std::tie(key, value) = trim(line);
-    baniya[key] = value;
-    // std::cout << key << "|"<< value << std::endl;
+    std::array<std::string, 2> temp{trim(line)};
+    baniya.emplace(temp[0], temp[1]);
   }
-
-  file.close();
   return baniya;
 }
+}; // namespace env
