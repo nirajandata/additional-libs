a37e78ce1df3828dd38169fe6d3aab57f58dc0cb
Navaraj Dhakal <53692745+nirajandata@users.noreply.github.com>
2024-01-04 05:30:18 -0500

    Chore: Refractor
    excluded trim from exporting

--- getenv.cxx
+++ getenv.cxx
@@ -6,37 +6,37 @@ module;
 #include <map>
 
 export module modenv;
-export namespace env {
 
 [[nodiscard]] auto trim(std::string &line) noexcept {
-  line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
-  std::array<int, 4> locate;
-  size_t count = 0;
-  for (size_t i = 0; i < line.size(); i++) {
-    if (line[i] == '\'' || line[i] == '"') {
-      locate[count] = i + 1;
-      if (count & 1)
-        locate[count]--;
+    line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
+    std::array<int, 4> locate;
+    size_t count = 0;
+    for (size_t i = 0; i < line.size(); i++) {
+        if (line[i] == '\'' || line[i] == '"') {
+            locate[count] = i + 1;
+            if (count & 1)
+                locate[count]--;
 
-      count++;
+            count++;
+        }
     }
-  }
-  std::string key, value;
-  key = line.substr(locate[0], locate[1] - locate[0]);
-  value = line.substr(locate[2], locate[3] - locate[2]);
-  return std::array<std::string, 2>{key, value};
+    std::string key, value;
+    key = line.substr(locate[0], locate[1] - locate[0]);
+    value = line.substr(locate[2], locate[3] - locate[2]);
+    return std::array<std::string, 2>{key, value};
 }
 
-[[nodiscard]] auto getenv(std::string path = ".env") noexcept {
-  std::ifstream file(path);
-  // we store the key and value in dictionary called baniya
-  std::map<std::string, std::string> baniya;
-  std::string line;
+export namespace env {
+    [[nodiscard]] auto getenv(std::string path = ".env") noexcept {
+        std::ifstream file(path);
+        // we store the key and value in dictionary called baniya
+        std::map<std::string, std::string> baniya;
+        std::string line;
 
-  while (getline(file, line)) {
-    std::array<std::string, 2> temp{trim(line)};
-    baniya.emplace(temp[0], temp[1]);
-  }
-  return baniya;
-}
-}; // namespace env
+        while (getline(file, line)) {
+            std::array<std::string, 2> temp{trim(line)};
+            baniya.emplace(temp[0], temp[1]);
+        }
+        return baniya;
+    }
+}; //
